e9ebc2124e5a5ec70a82e032d5f524b7addd222c
Robert Lindquist <dtrush@snet.net>
2020-01-04 20:13:51 -0500

    More fleshed out puzzle system, now uses Vector3's to control puzzlePart movement rather than animations.
    Player currently slides off of the moving platform in puzzle 3, need some system to stop this from happening.

--- Assets/Scripts/PuzzlePartScript.cs
+++ Assets/Scripts/PuzzlePartScript.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class PuzzlePartScript : MonoBehaviour
 {
-    [Tooltip("List of animation names that this puzzlePart can do, based on what color orb is used")] public List<string> actions;
+    [Tooltip("List of animation names that this puzzlePart can do, based on what color orb is used")] public List<Vector3> actions;
     [Tooltip("The pedestal that activates this puzzlePart")] public GameObject pedestal;
-    [Tooltip("The animator attached to this puzzlePart")] public Animator animator;
+    private Vector3 target;
+
+    private void Start()
+    {
+        target = transform.position;
+    }
 
     public void Activate(int OrbColor)
     {
@@ -14,11 +19,17 @@ public class PuzzlePartScript : MonoBehaviour
         {
             //Play animation with key OrbColor from dict actions
             Debug.Log("playAnimation" + OrbColor);
-            animator.SetTrigger(actions[OrbColor]);
+            //animator.SetTrigger(actions[OrbColor]);
+            target = actions[OrbColor];
         }
         else
         {
             Debug.Log("No animation " + OrbColor);
         }
+    }
+
+    private void Update()
+    {
+        transform.position = Vector3.Slerp(transform.position, target, 0.01f);
     }
 }
