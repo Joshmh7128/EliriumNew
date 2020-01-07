ee923200cfbbb6d217cc43c63aab75495c824c80
Robert Lindquist <dtrush@snet.net>
2020-01-06 20:29:02 -0500

    Color Changing for Orbs and Pedestals implemented.

--- Assets/Scripts/PedestalScript.cs
+++ Assets/Scripts/PedestalScript.cs
@@ -8,19 +8,23 @@ public class PedestalScript : MonoBehaviour
     [Tooltip("The orb detection trigger on this pedestal.")] public Collider orbDetect;
     [Tooltip("The list of puzzle objects that this pedestal activates")] public List<PuzzlePartScript> puzzleParts;
 
+    public List<Material> orbMats;
+    public enum pedColorList { None, Red, Green, Blue, Orange }; //what color is our orb?
+    public pedColorList pedColor; //put it in the editor
+    public int pedColorInt;
+    public MeshRenderer pedRend;
+    private Material targetMat;
+
     // Start is called before the first frame update
     void Start()
     {
-        if (puzzleParts.Count == 0)
-        {
-            Debug.Log("No puzzle parts!");
-        }
+        SetColor((int)pedColor);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        pedRend.material.Lerp(pedRend.material, targetMat, 0.2f);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -32,10 +36,19 @@ public class PedestalScript : MonoBehaviour
             {
                 activated.Activate(other.GetComponent<OrbScript>().orbColorInt);
             }
+            SetColor(other.GetComponent<OrbScript>().orbColorInt);
         }
         else
         {
             //Debug.Log("Not orb!");
         }
+    }
+
+    public void SetColor(int colorNum)
+    {
+        targetMat = orbMats[colorNum];
+
+        //cast the enum to an int
+        pedColorInt = colorNum;
     }
 }
