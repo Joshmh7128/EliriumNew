9aab1ff972e1b632a59578bfdbbe4be8f3198f60
Josh <joshmh7128@gmail.com>
2020-04-03 13:14:05 -0400

    Working on Area 7

---
+++ Assets/Scripts/ButterflyAnimControl.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButterflyAnimControl : MonoBehaviour
+{
+    public Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.speed = Random.Range(0.25f, 3f);
+    }
+}
