17059a3e2b82fb0f0949a15d0bbfbfbe2378a47e
Josh <joshmh7128@gmail.com>
2020-04-08 15:53:29 -0400

    Added Area 7 scene

---
+++ Assets/Scripts/FlowerTesting.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlowerTesting : MonoBehaviour
+{
+    public MeshRenderer meshRenderer;
+    public Material[] flowerMaterials; // the flower materials
+
+    void Start()
+    {
+        // randomly select a material
+        int choice = Random.Range(0, flowerMaterials.Length);
+        // get the materials
+        Material[] meshMaterials = meshRenderer.materials;
+        // setup a new modifiable material set
+        Material[] newMaterials = meshMaterials;
+        // modify the set
+        newMaterials[1] = flowerMaterials[choice];
+        // set the materials
+        meshRenderer.materials = newMaterials;
+    }
+}
