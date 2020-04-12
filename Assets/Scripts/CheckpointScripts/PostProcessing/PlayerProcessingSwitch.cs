d48a7b1e7c0d3e2e96ed933df1c60e97490d89cb
Robert Lindquist <dtrush@snet.net>
2020-04-12 17:17:54 -0400

    Caves Pre-Alpha

---
+++ Assets/Scripts/CheckpointScripts/PostProcessing/PlayerProcessingSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+public class PlayerProcessingSwitch : MonoBehaviour
+{
+    public PostProcessProfile loadProfile;
+    public GameObject player;
+
+    private void Start()
+    {
+        player = FindObjectOfType<CameraScript>().gameObject;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player.GetComponent<PostProcessVolume>().profile = loadProfile;
+        }
+    }
+}
